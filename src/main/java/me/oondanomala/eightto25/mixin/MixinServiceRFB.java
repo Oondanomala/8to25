e9967e58f1bc5fbb33daf9586d08f1759af06cac
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2026-08-14 16:12:58 +0200

    Manually load the mixin configuration file in dev

--- src/main/java/me/oondanomala/eightto25/mixin/MixinServiceRFB.java
+++ src/main/java/me/oondanomala/eightto25/mixin/MixinServiceRFB.java
@@ -1,11 +1,22 @@
 package me.oondanomala.eightto25.mixin;
 
+import net.minecraft.launchwrapper.Launch;
 import org.spongepowered.asm.mixin.MixinEnvironment.CompatibilityLevel;
+import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.service.IAdviceProvider;
 import org.spongepowered.asm.service.mojang.MixinServiceLaunchWrapper;
 
 @SuppressWarnings("unused")
 public class MixinServiceRFB extends MixinServiceLaunchWrapper {
+    @Override
+    public void init() {
+        super.init();
+        // Manually load the Mixin configuration in dev as it is not automatically found
+        if (Launch.blackboard.get("fml.deobfuscatedEnvironment") == Boolean.TRUE) {
+            Mixins.addConfiguration("8to25.mixins.json");
+        }
+    }
+
     @Override
     public String getName() {
         return "RetroFuturaBootstrap";
